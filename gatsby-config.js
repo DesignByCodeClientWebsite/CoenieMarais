622a2edc5aa5e44b40e1dfd7b7c9905086ef6ce8
Claude Myburgh <claudemyburgh@gmail.com>
2020-11-29 10:19:09 +0200

    Update gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    pathPrefix: '/coeniemarais',
+    pathPrefix: '/CoenieMarais',
     siteMetadata: {
         title: `Coenie Marais`,
         description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-        author: `DesignbyCode`,
+        author: `DesignByCode`,
     },
     plugins: [
         `gatsby-plugin-react-helmet`,
